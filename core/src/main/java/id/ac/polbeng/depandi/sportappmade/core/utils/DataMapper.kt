2519339161e1759e37573a5418dd0737c9ac3022
Depandi Enda <depandienda@gmail.com>
2021-03-23 10:59:19 +0700

    Add player utils

--- core/src/main/java/id/ac/polbeng/depandi/sportappmade/core/utils/DataMapper.kt
+++ core/src/main/java/id/ac/polbeng/depandi/sportappmade/core/utils/DataMapper.kt
@@ -1,7 +1,10 @@
 package id.ac.polbeng.depandi.sportappmade.core.utils
 
+import id.ac.polbeng.depandi.sportappmade.core.data.source.local.entity.PlayerEntity
 import id.ac.polbeng.depandi.sportappmade.core.data.source.local.entity.SportEntity
+import id.ac.polbeng.depandi.sportappmade.core.data.source.remote.response.PlayerResponse
 import id.ac.polbeng.depandi.sportappmade.core.data.source.remote.response.SportResponse
+import id.ac.polbeng.depandi.sportappmade.core.domain.model.Player
 import id.ac.polbeng.depandi.sportappmade.core.domain.model.Sport
 
 object DataMapper {
@@ -29,4 +32,76 @@ object DataMapper {
                 strSportDescription = it.strSportDescription
             )
         }
+    fun mapPlayerResponseToDomain(playerResponseList: List<PlayerResponse>): List<Player>{
+        val playerList = ArrayList<Player>()
+        playerResponseList.map {
+            val player = Player(
+                    idPlayer = it.idPlayer,
+                    idTeam = it.idTeam,
+                    strNationality = it.strNationality,
+                    strPlayer = it.strPlayer,
+                    strTeam = it.strTeam,
+                    strSport = it.strSport,
+                    dateBorn = it.dateBorn,
+                    strBirthLocation = it.strBirthLocation,
+                    strDescriptionEN = it.strDescriptionEN,
+                    strGender = it.strGender,
+                    strPosition = it.strPosition,
+                    strFacebook = it.strFacebook,
+                    strTwitter = it.strTwitter,
+                    strInstagram = it.strInstagram,
+                    strHeight = it.strHeight,
+                    strWeight = it.strWeight,
+                    strThumb = it.strThumb
+            )
+            playerList.add(player)
+        }
+        return playerList
+    }
+    fun mapPlayerDomainToEntity(player: Player) : PlayerEntity =
+            PlayerEntity(
+                    idPlayer = player.idPlayer,
+                    idTeam = player.idTeam,
+                    strNationality = player.strNationality,
+                    strPlayer = player.strPlayer,
+                    strTeam = player.strTeam,
+                    strSport = player.strSport,
+                    dateBorn = player.dateBorn,
+                    strBirthLocation = player.strBirthLocation,
+                    strDescriptionEN = player.strDescriptionEN,
+                    strGender = player.strGender,
+                    strPosition = player.strPosition,
+                    strFacebook = player.strFacebook,
+                    strTwitter = player.strTwitter,
+                    strInstagram = player.strInstagram,
+                    strHeight = player.strHeight,
+                    strWeight = player.strWeight,
+                    strThumb = player.strThumb
+            )
+    fun mapPlayerEntitiesToDomain(playerEntities: List<PlayerEntity>) : List<Player> {
+        val playerList = ArrayList<Player>()
+        playerEntities.map {
+            val player = Player(
+                    idPlayer = it.idPlayer,
+                    idTeam = it.idTeam,
+                    strNationality = it.strNationality,
+                    strPlayer = it.strPlayer,
+                    strTeam = it.strTeam,
+                    strSport = it.strSport,
+                    dateBorn = it.dateBorn,
+                    strBirthLocation = it.strBirthLocation,
+                    strDescriptionEN = it.strDescriptionEN,
+                    strGender = it.strGender,
+                    strPosition = it.strPosition,
+                    strFacebook = it.strFacebook,
+                    strTwitter = it.strTwitter,
+                    strInstagram = it.strInstagram,
+                    strHeight = it.strHeight,
+                    strWeight = it.strWeight,
+                    strThumb = it.strThumb
+            )
+            playerList.add(player)
+        }
+        return playerList
+    }
 }
