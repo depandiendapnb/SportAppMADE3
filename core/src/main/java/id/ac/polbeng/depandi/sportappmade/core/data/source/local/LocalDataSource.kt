b16688f769373ebb10f81fbd4c67400b4eb660ea
Depandi Enda <depandienda@gmail.com>
2021-03-23 10:30:11 +0700

    Add player local data source (insert player & delete favorite player)

--- core/src/main/java/id/ac/polbeng/depandi/sportappmade/core/data/source/local/LocalDataSource.kt
+++ core/src/main/java/id/ac/polbeng/depandi/sportappmade/core/data/source/local/LocalDataSource.kt
@@ -9,4 +9,6 @@ class LocalDataSource(private val sportDao: SportDao) {
     fun getAllSport(): Flow<List<SportEntity>> = sportDao.getAllSport()
     suspend fun insertListSport(sportList: List<SportEntity>) = sportDao.insertListSport(sportList)
     fun getAllFavoritePlayer(): Flow<List<PlayerEntity>> = sportDao.getAllFavoritePlayer()
+    suspend fun insertFavoritePlayer(player: PlayerEntity) = sportDao.insertFavoritePlayer(player)
+    fun deleteFavoritePlayer(player: PlayerEntity) = sportDao.deleteFavoritePlayer(player)
 }
--- core/src/main/java/id/ac/polbeng/depandi/sportappmade/core/data/source/local/room/SportDao.kt
+++ core/src/main/java/id/ac/polbeng/depandi/sportappmade/core/data/source/local/room/SportDao.kt
@@ -1,9 +1,6 @@
 package id.ac.polbeng.depandi.sportappmade.core.data.source.local.room
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import id.ac.polbeng.depandi.sportappmade.core.data.source.local.entity.PlayerEntity
 import id.ac.polbeng.depandi.sportappmade.core.data.source.local.entity.SportEntity
 import kotlinx.coroutines.flow.Flow
@@ -18,4 +15,10 @@ interface SportDao {
 
     @Query("SELECT * FROM tb_favorite_player")
     fun getAllFavoritePlayer(): Flow<List<PlayerEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoritePlayer(player: PlayerEntity)
+
+    @Delete
+    fun deleteFavoritePlayer(player: PlayerEntity)
 }
