6d6a21cd87a4aa2eef26ddbe27468339c575fc16
Depandi Enda <depandienda@gmail.com>
2021-03-23 13:34:37 +0700

    Add favorite player

--- app/src/main/java/id/ac/polbeng/depandi/sportappmade/ui/favorite_player/FavoritePlayerFragment.kt
+++ app/src/main/java/id/ac/polbeng/depandi/sportappmade/ui/favorite_player/FavoritePlayerFragment.kt
@@ -1,29 +1,88 @@
 package id.ac.polbeng.depandi.sportappmade.ui.favorite_player
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.ac.polbeng.depandi.sportappmade.R
+import id.ac.polbeng.depandi.sportappmade.core.domain.model.Player
+import id.ac.polbeng.depandi.sportappmade.core.ui.PlayerAdapter
+import id.ac.polbeng.depandi.sportappmade.core.ui.PlayerFragmentCallback
+import id.ac.polbeng.depandi.sportappmade.databinding.FragmentFavoritePlayerBinding
+import id.ac.polbeng.depandi.sportappmade.ui.detail_player.DetailPlayerActivity
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class FavoritePlayerFragment : Fragment() {
+class FavoritePlayerFragment : Fragment(), PlayerFragmentCallback {
 
-    private val favoritePlayerViewModel: FavoritePlayerViewModel by viewModel()
+    private val favoriteViewModel: FavoritePlayerViewModel by viewModel()
+
+    private var _binding: FragmentFavoritePlayerBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_favorite_player, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        favoritePlayerViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    ): View {
+        _binding = FragmentFavoritePlayerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (activity != null) {
+            val playerAdapter = PlayerAdapter(this)
+            with(binding.rvPlayer) {
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                adapter = playerAdapter
+            }
+            showLoading(true)
+            favoriteViewModel.listPlayer.observe(viewLifecycleOwner, { result ->
+                showLoading(false)
+                if (result != null) {
+                    playerAdapter.setPlayerList(result)
+                    if(result.isEmpty()){
+                        showInfo(true, getString(R.string.data_empty))
+                    }else{
+                        showInfo(false, "")
+                    }
+                }
+            })
+        }
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            binding.viewInfo.root.visibility = View.GONE
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+
+    private fun showInfo(state: Boolean, info: String){
+        if (state) {
+            binding.rvPlayer.visibility = View.GONE
+            binding.viewInfo.root.visibility = View.VISIBLE
+            binding.viewInfo.tvInfo.text = info
+        } else {
+            binding.viewInfo.root.visibility = View.GONE
+            binding.rvPlayer.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onItemClick(player: Player) {
+        val intent = Intent(activity, DetailPlayerActivity::class.java)
+        intent.putExtra(DetailPlayerActivity.EXTRA_DATA, player)
+        startActivity(intent)
     }
 }
--- app/src/main/java/id/ac/polbeng/depandi/sportappmade/ui/favorite_player/FavoritePlayerViewModel.kt
+++ app/src/main/java/id/ac/polbeng/depandi/sportappmade/ui/favorite_player/FavoritePlayerViewModel.kt
@@ -1,14 +1,9 @@
 package id.ac.polbeng.depandi.sportappmade.ui.favorite_player
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import id.ac.polbeng.depandi.sportappmade.core.domain.usecase.SportUseCase
 
 class FavoritePlayerViewModel(sportUseCase: SportUseCase) : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
-    }
-    val text: LiveData<String> = _text
+    val listPlayer = sportUseCase.getAllFavoritePlayer().asLiveData()
 }
