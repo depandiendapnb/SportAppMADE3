51acb9b70bdde0c14f4635d88edc78c87f19da77
Depandi Enda <depandienda@gmail.com>
2021-04-08 21:10:25 +0700

    Remove double bang operator at SportFragment.kt

--- app/src/main/java/id/ac/polbeng/depandi/sportappmade/ui/sport/SportFragment.kt
+++ app/src/main/java/id/ac/polbeng/depandi/sportappmade/ui/sport/SportFragment.kt
@@ -19,63 +19,61 @@ class SportFragment : Fragment(), SportFragmentCallback {
     private val sportViewModel: SportViewModel by viewModel()
 
     private var _binding: FragmentSportBinding? = null
-    private val binding get() = _binding!!
+    private val binding get() = _binding
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         _binding = FragmentSportBinding.inflate(inflater, container, false)
-        return binding.root
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (activity != null) {
-            val sportAdapter = SportAdapter(this)
-            with(binding.rvSport) {
-                layoutManager = LinearLayoutManager(context)
-                setHasFixedSize(true)
-                adapter = sportAdapter
-            }
-            sportViewModel.sportList.observe(viewLifecycleOwner, { sports ->
-                if (sports != null) {
-                    when (sports) {
-                        is Resource.Loading -> {
-                            showLoading(true)
-                        }
-                        is Resource.Success -> {
-                            showLoading(false)
-                            showInfo(false)
-                            sportAdapter.setSportList(sports.data)
-                        }
-                        is Resource.Error -> {
-                            showLoading(false)
-                            showInfo(true)
-                        }
+        val sportAdapter = SportAdapter(this)
+        with(binding?.rvSport) {
+            this?.layoutManager = LinearLayoutManager(context)
+            this?.setHasFixedSize(true)
+            this?.adapter = sportAdapter
+        }
+        sportViewModel.sportList.observe(viewLifecycleOwner, { sports ->
+            if (sports != null) {
+                when (sports) {
+                    is Resource.Loading -> {
+                        showLoading(true)
+                    }
+                    is Resource.Success -> {
+                        showLoading(false)
+                        showInfo(false)
+                        sportAdapter.setSportList(sports.data)
+                    }
+                    is Resource.Error -> {
+                        showLoading(false)
+                        showInfo(true)
                     }
                 }
-            })
-        }
+            }
+        })
     }
 
     private fun showLoading(state: Boolean) {
         if (state) {
-            binding.progressBar.visibility = View.VISIBLE
+            binding?.progressBar?.visibility = View.VISIBLE
         } else {
-            binding.progressBar.visibility = View.GONE
+            binding?.progressBar?.visibility = View.GONE
         }
     }
 
     private fun showInfo(state: Boolean){
         if (state) {
-            binding.rvSport.visibility = View.GONE
-            binding.viewInfo.root.visibility = View.VISIBLE
-            binding.viewInfo.tvInfo.text = getString(R.string.data_empty)
+            binding?.rvSport?.visibility = View.GONE
+            binding?.viewInfo?.root?.visibility = View.VISIBLE
+            binding?.viewInfo?.tvInfo?.text = getString(R.string.data_empty)
         } else {
-            binding.rvSport.visibility = View.VISIBLE
-            binding.viewInfo.root.visibility = View.GONE
+            binding?.rvSport?.visibility = View.VISIBLE
+            binding?.viewInfo?.root?.visibility = View.GONE
         }
     }
 
