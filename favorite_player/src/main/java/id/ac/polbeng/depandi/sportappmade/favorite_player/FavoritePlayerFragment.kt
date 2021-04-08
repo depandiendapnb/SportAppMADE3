d5d2b65082d1f20cf82121da3bc297dd68e39d60
Depandi Enda <depandienda@gmail.com>
2021-04-08 21:03:35 +0700

    Remove double bang operator

--- favorite_player/src/main/java/id/ac/polbeng/depandi/sportappmade/favorite_player/FavoritePlayerFragment.kt
+++ favorite_player/src/main/java/id/ac/polbeng/depandi/sportappmade/favorite_player/FavoritePlayerFragment.kt
@@ -16,70 +16,70 @@ import id.ac.polbeng.depandi.sportappmade.favorite_player.databinding.FragmentFa
 import id.ac.polbeng.depandi.sportappmade.ui.detail_player.DetailPlayerActivity
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.context.loadKoinModules
+import org.koin.core.context.unloadKoinModules
 
 class FavoritePlayerFragment : Fragment(), PlayerFragmentCallback {
 
     private val favoriteViewModel: FavoritePlayerViewModel by viewModel()
 
     private var _binding: FragmentFavoritePlayerBinding? = null
-    private val binding get() = _binding!!
+    private val binding get() = _binding
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         loadKoinModules(favoritePlayerModule)
         _binding = FragmentFavoritePlayerBinding.inflate(inflater, container, false)
-        return binding.root
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (activity != null) {
-            val playerAdapter = PlayerAdapter(this)
-            with(binding.rvPlayer) {
-                layoutManager = LinearLayoutManager(context)
-                setHasFixedSize(true)
-                adapter = playerAdapter
-            }
-            showLoading(true)
-            favoriteViewModel.listPlayer.observe(viewLifecycleOwner, { result ->
-                showLoading(false)
-                if (result != null) {
-                    playerAdapter.setPlayerList(result)
-                    if(result.isEmpty()){
-                        showInfo(true, getString(R.string.data_empty))
-                    }else{
-                        showInfo(false, "")
-                    }
-                }
-            })
+        val playerAdapter = PlayerAdapter(this)
+        with(binding?.rvPlayer) {
+            this?.layoutManager = LinearLayoutManager(context)
+            this?.setHasFixedSize(true)
+            this?.adapter = playerAdapter
         }
+        showLoading(true)
+        favoriteViewModel.listPlayer.observe(viewLifecycleOwner, { result ->
+            showLoading(false)
+            if (result != null) {
+                playerAdapter.setPlayerList(result)
+                if(result.isEmpty()){
+                    showInfo(true, getString(R.string.data_empty))
+                }else{
+                    showInfo(false, "")
+                }
+            }
+        })
     }
 
     private fun showLoading(state: Boolean) {
         if (state) {
-            binding.viewInfo.root.visibility = View.GONE
-            binding.progressBar.visibility = View.VISIBLE
+            binding?.viewInfo?.root?.visibility = View.GONE
+            binding?.progressBar?.visibility = View.VISIBLE
         } else {
-            binding.progressBar.visibility = View.GONE
+            binding?.progressBar?.visibility = View.GONE
         }
     }
 
     private fun showInfo(state: Boolean, info: String){
         if (state) {
-            binding.rvPlayer.visibility = View.GONE
-            binding.viewInfo.root.visibility = View.VISIBLE
-            binding.viewInfo.tvInfo.text = info
+            binding?.rvPlayer?.visibility = View.GONE
+            binding?.viewInfo?.root?.visibility = View.VISIBLE
+            binding?.viewInfo?.tvInfo?.text = info
         } else {
-            binding.viewInfo.root.visibility = View.GONE
-            binding.rvPlayer.visibility = View.VISIBLE
+            binding?.viewInfo?.root?.visibility = View.GONE
+            binding?.rvPlayer?.visibility = View.VISIBLE
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        unloadKoinModules(favoritePlayerModule)
         _binding = null
     }
 
