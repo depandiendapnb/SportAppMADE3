1690cb13da1369851e0b5d1bb39ab52901291b55
Depandi Enda <depandienda@gmail.com>
2021-04-08 21:08:03 +0700

    Remove double bang operator at PlayerFragment.kt

--- app/src/main/java/id/ac/polbeng/depandi/sportappmade/ui/player/PlayerFragment.kt
+++ app/src/main/java/id/ac/polbeng/depandi/sportappmade/ui/player/PlayerFragment.kt
@@ -23,75 +23,73 @@ class PlayerFragment : Fragment(), PlayerFragmentCallback {
     private val playerViewModel: PlayerViewModel by viewModel()
 
     private var _binding: FragmentPlayerBinding? = null
-    private val binding get() = _binding!!
+    private val binding get() = _binding
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         _binding = FragmentPlayerBinding.inflate(inflater, container, false)
-        return binding.root
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (activity != null) {
-            val playerAdapter = PlayerAdapter(this)
-            with(binding.rvPlayer) {
-                layoutManager = LinearLayoutManager(context)
-                setHasFixedSize(true)
-                adapter = playerAdapter
+        val playerAdapter = PlayerAdapter(this)
+        with(binding?.rvPlayer) {
+            this?.layoutManager = LinearLayoutManager(context)
+            this?.setHasFixedSize(true)
+            this?.adapter = playerAdapter
+        }
+
+        showInfo(true, getString(R.string.player_search))
+
+        binding?.btnSearch?.setOnClickListener {
+            val query = binding?.etSearch?.text?.trim().toString()
+            if (query == ""){
+                binding?.root?.let { it1 -> Snackbar.make(it1, getString(R.string.player_search), Snackbar.LENGTH_SHORT).show() }
+                return@setOnClickListener
             }
-
-            showInfo(true, getString(R.string.player_search))
-
-            binding.btnSearch.setOnClickListener {
-                val query = binding.etSearch.text.trim().toString()
-                if (query == ""){
-                    Snackbar.make(binding.root, getString(R.string.player_search), Snackbar.LENGTH_SHORT).show()
-                    return@setOnClickListener
-                }
-                val searchQuery = query.replace(" ", "_")
-                playerViewModel.getPlayerByName(searchQuery)
-                playerViewModel.listPlayer.observe(viewLifecycleOwner, { result ->
-                    Log.d("PlayerFragment", result.data.toString())
-                    if (result != null) {
-                        when (result) {
-                            is Resource.Loading -> showLoading(true)
-                            is Resource.Success -> {
-                                showLoading(false)
-                                showInfo(false, "")
-                                playerAdapter.setPlayerList(result.data)
-                            }
-                            is Resource.Error -> {
-                                showLoading(false)
-                                showInfo(true, getString(R.string.data_empty))
-                            }
+            val searchQuery = query.replace(" ", "_")
+            playerViewModel.getPlayerByName(searchQuery)
+            playerViewModel.listPlayer.observe(viewLifecycleOwner, { result ->
+                Log.d("PlayerFragment", result.data.toString())
+                if (result != null) {
+                    when (result) {
+                        is Resource.Loading -> showLoading(true)
+                        is Resource.Success -> {
+                            showLoading(false)
+                            showInfo(false, "")
+                            playerAdapter.setPlayerList(result.data)
+                        }
+                        is Resource.Error -> {
+                            showLoading(false)
+                            showInfo(true, getString(R.string.data_empty))
                         }
                     }
-                })
-            }
+                }
+            })
         }
     }
 
     private fun showLoading(state: Boolean) {
         if (state) {
-            binding.viewInfo.root.visibility = View.GONE
-            binding.progressBar.visibility = View.VISIBLE
+            binding?.viewInfo?.root?.visibility = View.GONE
+            binding?.progressBar?.visibility = View.VISIBLE
         } else {
-            binding.progressBar.visibility = View.GONE
+            binding?.progressBar?.visibility = View.GONE
         }
     }
 
     private fun showInfo(state: Boolean, info: String){
         if (state) {
-            binding.rvPlayer.visibility = View.GONE
-            binding.viewInfo.root.visibility = View.VISIBLE
-            binding.viewInfo.tvInfo.text = info
+            binding?.rvPlayer?.visibility = View.GONE
+            binding?.viewInfo?.root?.visibility = View.VISIBLE
+            binding?.viewInfo?.tvInfo?.text = info
         } else {
-            binding.viewInfo.root.visibility = View.GONE
-            binding.rvPlayer.visibility = View.VISIBLE
+            binding?.viewInfo?.root?.visibility = View.GONE
+            binding?.rvPlayer?.visibility = View.VISIBLE
         }
     }
 
